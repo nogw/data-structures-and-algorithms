540547f1731b8f30159bc4140b690c1ebbc28fb6
nogw <gabrielnogueiraoliveira@gmail.com>
2023-02-27 02:48:41 -0300

    feat/golang: LeetCode 234. Palindrome Linked List

--- elixir/algorithms/isomorphic_strings/isomorphic_strings.exs
+++ elixir/algorithms/isomorphic_strings/isomorphic_strings.exs
@@ -1,22 +1,32 @@
+defmodule ListNode do
+  @type t :: %__MODULE__{val: integer, next: ListNode.t() | nil}
+  defstruct val: 0, next: nil
+end
+
 defmodule Solution do
-  @spec is_isomorphic(s :: String.t(), t :: String.t()) :: boolean
-  def is_isomorphic(s, t) do
-    morph(s) == morph(t)
+  def is_palindrome(list) do
+    {reversed_head, length} = reverse(list, nil, 0)
+    half = div(length, 2)
+    check(list, reversed_head, 0, half)
   end
 
-  def morph(s) do
-    {encoded_chars, _} = Enum.map_reduce(String.graphemes(s), %{}, &map_char/2)
-    Enum.join(encoded_chars)
+  def reverse(list, reversed_head \\ nil, i) do
+    if list.next == nil do
+      {%ListNode{list | next: reversed_head}, i + 1}
+    else
+      reverse(list.next, %ListNode{list | next: reversed_head}, i + 1)
+    end
   end
 
-  defp map_char(char, char_mapper) do
-    case Map.has_key?(char_mapper, char) do
-      true ->
-        {char_mapper[char], char_mapper}
+  def check(list, reversed_head, i, len) when i == len do
+    list.val == reversed_head.val
+  end
 
-      false ->
-        cur_letter_idx = map_size(char_mapper)
-        {cur_letter_idx + 1, Map.put(char_mapper, char, cur_letter_idx + 1)}
+  def check(list, reversed_head, i, len) do
+    if list.val != reversed_head.val do
+      false
+    else
+      check(list.next, reversed_head.next, i + 1, len)
     end
   end
 end
